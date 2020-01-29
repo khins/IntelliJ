759897c509d09e39b9819e7a664b5ce6a5c0fdc3
kevin <kevin@hinshawfam.com>
2020-01-29 06:24:22 -0600

    Stack unwinding and obtaining data from an exception object

--- ExceptionHandling/src/UsingExceptions.java
+++ ExceptionHandling/src/UsingExceptions.java
@@ -1,16 +1,29 @@
 /*
  try...catch...finally exception-handling mechanism
  https://learning.oreilly.com/videos/java-8-fundamentals/9780133489354/9780133489354-JFUN_lesson11_04
+ adds Stack unwinding and obtaining data from an exception object
+ video https://learning.oreilly.com/videos/java-8-fundamentals/9780133489354/9780133489354-JFUN_lesson11_05?autoplay=false
 */
 public class UsingExceptions {
     public static void main(String[] args) {
         try {
-            throwException();
+            method1();
         } catch (Exception e) {
             System.err.println("Exception handled in main");
+            e.printStackTrace();
+            StackTraceElement[] traceElements = e.getStackTrace();
+            System.out.printf("%nStack trace from getStackTrace:%n");
+            System.out.println("Class\t\tFile\t\t\tLine\tMethod");
+
+            for (StackTraceElement element : traceElements) {
+                System.out.printf("%s\t", element.getClassName());
+                System.out.printf("%s\t", element.getFileName());
+                System.out.printf("%s\t", element.getLineNumber());
+                System.out.printf("%s%n", element.getMethodName());
+            }
         }
 
-        doesNotThrowException();
+//        doesNotThrowException();
     }
 
     private static void doesNotThrowException() {
@@ -34,5 +47,17 @@ public class UsingExceptions {
         } finally {
             System.err.println("Finally executed");
         }
+    }
+
+    public static void method1() throws Exception {
+        method2();
+    }
+
+    private static void method2() throws Exception {
+        method3();
+    }
+
+    private static void method3() throws Exception {
+        throw new Exception("exception in method3");
     }
 }
